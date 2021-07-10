b179b52270ff085f6698b0276484c29b97703ead
Yuchen <lyuchen@fb.com>
2021-07-10 19:14:11 +0000

    seems to be working

--- scripts/stl_single_node.sh
+++ scripts/stl_single_node.sh
@@ -6,15 +6,16 @@
 #SBATCH --nodes=1
 #SBATCH --time=12:00:00
 #SBATCH --signal=SIGUSR1@120
+#SBATCH --exclude=a100-st-p4d24xlarge-8
 
 DATASET_PATH="/data/home/lyuchen/swav_exp/new_stl10"
-EXPERIMENT_PATH="./experiments/stl/srun_deepcluster_knn5_epoch20_new"
+EXPERIMENT_PATH="./experiments/stl/srun_deepcluster_knn0_lr1.2"
 mkdir -p $EXPERIMENT_PATH
 
 srun --output=${EXPERIMENT_PATH}/%j.out --error=${EXPERIMENT_PATH}/%j.err --label \
 python -u main_deepclusterv2.py \
---knn_epoch 20 \
---nb_neighbor 5 \
+--knn_epoch 0 \
+--nb_neighbor 0 \
 --data_path $DATASET_PATH \
 --nmb_crops 2 \
 --size_crops 96 \
@@ -27,8 +28,8 @@ python -u main_deepclusterv2.py \
 --nmb_prototypes 512 \
 --epochs 400 \
 --batch_size 64 \
---base_lr 4.8 \
---final_lr 0.0048 \
+--base_lr 1.2 \
+--final_lr 0.0012 \
 --freeze_prototypes_niters 40000 \
 --wd 0.000001 \
 --warmup_epochs 10 \
