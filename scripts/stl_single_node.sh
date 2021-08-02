0edb6e7cf73964f746da6437fdf2eb6989e5d8cf
Yuchen <lyuchen@fb.com>
2021-08-02 20:26:52 +0000

    exclude some node

--- scripts/stl_single_node.sh
+++ scripts/stl_single_node.sh
@@ -6,16 +6,16 @@
 #SBATCH --nodes=1
 #SBATCH --time=12:00:00
 #SBATCH --signal=SIGUSR1@120
-#SBATCH --exclude=a100-st-p4d24xlarge-35
+#SBATCH --exclude=a100-st-p4d24xlarge-35,a100-st-p4d24xlarge-27,a100-st-p4d24xlarge-34
 
 DATASET_PATH="/data/home/lyuchen/swav_exp/new_stl10"
-EXPERIMENT_PATH="./experiments/stl/srun_deepcluster_knn0_epoch0_lr1.2_new"
+EXPERIMENT_PATH="./experiments/stl/srun_deepcluster_knn1_epoch0_lr1.2_new"
 mkdir -p $EXPERIMENT_PATH
 
 srun --output=${EXPERIMENT_PATH}/%j.out --error=${EXPERIMENT_PATH}/%j.err --label \
 python -u main_deepclusterv2.py \
 --knn_epoch 0 \
---nb_neighbor 0 \
+--nb_neighbor 1 \
 --data_path $DATASET_PATH \
 --nmb_crops 2 \
 --size_crops 96 \
